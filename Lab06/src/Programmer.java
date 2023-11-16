11d32670ee8f0729dc73ebde3a9347c2efa74a4a
andresporrasdev <camilop1730@gmail.com>
2023-11-16 11:35:53 -0500

    Added method overloading to five param constructor

--- Lab06/src/Programmer.java
+++ Lab06/src/Programmer.java
@@ -1,11 +1,14 @@
-
-// LAB 6: YOU ARE REQUIRED TO MODIFY THIS CODE!!!
-
+/*
+* Title Assessment: Lab06
+* Student Name: Andres Camilo Porras Becerra
+* Due Date: 19 Nov
+* Description: This program shows polymorphism and interfaces by applying a method to objects of different classes. 
+* Professor Name: Islam Gomaa
+* Course CST8284 Section 331
+*/
 //Programmer.java
 
 //Programmer - This is an abstract superclass that IMPLEMENTS the Payme interface.
-
-//SOME MODIFICATION ARE REQUIRED IN THIS SECTION OF YOUR HERE - LOOK CLOSELY!!!
 
 public abstract class Programmer implements Payme {
 
@@ -22,39 +25,58 @@ public Programmer(String first, String last, String ssn) {
  socialSecurityNumber = ssn;
 }
 
-//five-argument constructor
+//five-argument method overloading constructor
 public Programmer(String first, String last, String ssn, int month, int year) {
-firstName = first;
-lastName = last;
-socialSecurityNumber = ssn;
+this(first, last, ssn);
 month = this.month;
 year = this.year;
 }
-/** CHANGES ARE REQUIRED IN THIS SECTION. 
-* THINK OF THE BEST WAY TO ACCESS PRIVATE VARIABLES.
-* IN THIS PORTION OF YOUR CODE INCLUDE ALL OF SUCH THAT APPLIES.
-*/
 
+/**
+ * Get name of programmer
+ * @return firstName Name of programmer
+ */
 public String getFirstName() {
 	return firstName;
 }
 
+/**
+ * Set name of programmer
+ * @param firstName Name of programmer
+ */
 public void setFirstName(String firstName) {
 	this.firstName = firstName;
 }
 
+/**
+ * Get last name of programmer
+ * @return lastName Return last name of programmer
+ */
 public String getLastName() {
 	return lastName;
 }
 
+/**
+ * Set last name programmer
+ * @param lastName Last name programmer
+ */
 public void setLastName(String lastName) {
 	this.lastName = lastName;
 }
 
+/**
+ * Get social security Number
+ * @return social security number of programmer
+ */
 public String getSocialSecurityNumber() {
 	return socialSecurityNumber;
 }
 
+
+/**
+ * Set social security number of programmer
+ * @param socialSecurityNumber security number of programmer
+ */
 public void setSocialSecurityNumber(String socialSecurityNumber) {
 	this.socialSecurityNumber = socialSecurityNumber;
 }
